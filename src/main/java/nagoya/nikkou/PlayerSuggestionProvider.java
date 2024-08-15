e2aa17a363314002f8dce6929bc490cc5f9c2a2c
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2024-08-16 05:53:04 +0900

    プロジェクトの修正

--- src/main/java/nagoya/nikkou/PlayerSuggestionProvider.java
+++ src/main/java/nagoya/nikkou/PlayerSuggestionProvider.java
@@ -1,2 +1,20 @@
-package nagoya.nikkou;public class PlayerSuggestionProvider {
+package nagoya.nikkou;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+public class PlayerSuggestionProvider {
+    public static CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) {
+        Collection<ServerPlayerEntity> players = context.getSource().getServer().getPlayerManager().getPlayerList();
+        for (ServerPlayerEntity player : players) {
+            builder.suggest(player.getName().getString());
+        }
+        return builder.buildFuture();
+    }
 }
--- src/main/java/nagoya/nikkou/whereplayer.java
+++ src/main/java/nagoya/nikkou/whereplayer.java
@@ -1,10 +1,65 @@
 package nagoya.nikkou;
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.command.CommandRegistryAccess;
+
+import java.util.function.Supplier;
 
 public class whereplayer implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            registerCommands(dispatcher, registryAccess);
+        });
+    }
+
+    private void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess) {
+        dispatcher.register(CommandManager.literal("where")
+                .then(CommandManager.argument("Player", StringArgumentType.string())
+                        .suggests(PlayerSuggestionProvider::getSuggestions)
+                        .executes(this::executeWhereCommand)));
+    }
+
+    private int executeWhereCommand(CommandContext<ServerCommandSource> context) {
+        String playerName = StringArgumentType.getString(context, "Player");
+        ServerPlayerEntity player = context.getSource().getServer().getPlayerManager().getPlayer(playerName);
+
+        if (player == null) {
+            context.getSource().sendError(Text.literal("そのプレイヤーは存在しません！"));
+            return 0;
+        }
+
+        StatusEffectInstance glowingEffect = new StatusEffectInstance(
+                StatusEffects.GLOWING,
+                1200,
+                255,
+                true,
+                true
+        );
+
+        player.addStatusEffect(glowingEffect);
+
+        int x = (int) player.getX();
+        int y = (int) player.getY();
+        int z = (int) player.getZ();
+
+        context.getSource().sendFeedback(
+                (Supplier<Text>) () -> Text.literal(playerName + " is at [" + x + ", " + y + ", " + z + "]").formatted(Formatting.AQUA),
+                false
+        );
+
+        return 1;
     }
 }
