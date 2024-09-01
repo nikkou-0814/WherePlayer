8e2f10dfa7699ea18d7611c743d6cc130f937c9f
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2024-09-02 04:47:13 +0900

    xaero-waypointのaddが押せない問題を修正、言語を日本語に変更

--- src/main/java/nagoya/nikkou/whereplayer.java
+++ src/main/java/nagoya/nikkou/whereplayer.java
@@ -5,23 +5,29 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.command.CommandRegistryAccess;
-
-import java.util.function.Supplier;
+import net.minecraft.util.Formatting;
 
 public class whereplayer implements ModInitializer {
+
+    private MinecraftServer server;
 
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             registerCommands(dispatcher, registryAccess);
+        });
+
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            this.server = server;
         });
     }
 
@@ -32,9 +38,20 @@ public class whereplayer implements ModInitializer {
                         .executes(this::executeWhereCommand)));
     }
 
+    private void send_text(String message) {
+        if (server != null && server.getPlayerManager() != null) {
+            Text coloredMessage = Text.literal(message).formatted(Formatting.AQUA);
+            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+                player.sendMessage(coloredMessage, false);
+            }
+        }
+    }
+
     private int executeWhereCommand(CommandContext<ServerCommandSource> context) {
         String playerName = StringArgumentType.getString(context, "Player");
         ServerPlayerEntity player = context.getSource().getServer().getPlayerManager().getPlayer(playerName);
+        ServerCommandSource source = context.getSource();
+        MinecraftServer server = source.getServer();
 
         if (player == null) {
             context.getSource().sendError(Text.literal("そのプレイヤーは存在しません"));
@@ -56,17 +73,8 @@ public class whereplayer implements ModInitializer {
         int z = (int) player.getZ();
         String dimension = player.getWorld().getRegistryKey().getValue().toString().replace("minecraft:", "");
 
-        Text positionMessage = Text.literal(playerName + " is at [" + x + ", " + y + ", " + z + "] in dimension " + dimension).formatted(Formatting.AQUA);
-        Text waypointMessage = Text.literal("xaero-waypoint:" + playerName + " is here" + ":S:" + x + ":" + y + ":" + z + ":3:false:0:Internal-" + dimension + "-waypoints");
-
-        context.getSource().sendFeedback(
-                (Supplier<Text>) () -> positionMessage,
-                true
-        );
-        context.getSource().sendFeedback(
-                (Supplier<Text>) () -> waypointMessage,
-                true
-        );
+        send_text(playerName + " は座標 [" + x + ", " + y + ", " + z + "] にいます。\nバイオーム: " + dimension);
+        send_text("xaero-waypoint:" + playerName + " is here" + ":S:" + x + ":" + y + ":" + z + ":3:false:0:Internal-" + dimension + "-waypoints");
 
         return 1;
     }
